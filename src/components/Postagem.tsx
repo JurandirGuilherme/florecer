5540d01daeabff9b891723b728ea717dbcc7a541
Jurandir Guilherme <jurandirguilherme1@gmail.com>
2024-05-27 17:47:29 -0300

    alteração de layout

--- src/components/Postagem.tsx
+++ src/components/Postagem.tsx
@@ -1,12 +1,25 @@
-import anel from "../assets/anel.png"
+import anel from "../assets/anel.png";
 
-export default function Postagem({ data}) {
-    return (
+export default function Postagem({ data }) {
+  return (
     <>
-    <div className="flex flex-col text-center">
-    <img src={data.imagem} className="max-h-72 max-w-72" alt="anel" />
-    <div>{data.nome}</div>
-    </div>
+      <div className="flex flex-col lg:overflow-x-auto lg:flex-row overflow-auto scrollbar-thin h-96 lg:space-x-10 items-center ">
+        {data.imagem.map((index) => (
+        <div key={index.alt}  className="flex space-x-4">
+          <img className="xl:w-52 xl:h-52" src={index.img} alt={index.alt} />
+          <div id="produto" className="flex-col flex justify-between ">
+            <div>
+              <h1>{index.nome}</h1>
+              <p>Descricao</p>
+            </div>
+            <div>
+              <li className="list-none">{index.preco}</li>
+              <button className="xl:p-4 xl:px-12 rounded hover:bg-pink-300 bg-pink-200 xl:text-lg font-thin">Comprar</button>
+            </div>
+          </div>
+        </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,149 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
+
+//Aneis
 import anel from "../assets/anel.png";
-import colar from "../assets/colar.png";
-import pulseira from "../assets/pulseira.png";
+import anel1 from "../assets/produtos/anel1.jpg"
+import anel2 from "../assets/produtos/anel2.jpg"
+import anel3 from "../assets/produtos/anel3.png"
+import anel4 from "../assets/produtos/anel4.png"
+
+
+
+//Colares
+
+import colar1 from "../assets/produtos/colar1.jpg"
+import colar2 from "../assets/produtos/colar2.webp"
+import colar3 from "../assets/produtos/colar3.webp"
+
+// Pulseiras
+
+
+import pulseira1 from "../assets/produtos/pulseira1.png"
+import pulseira2 from "../assets/produtos/pulseira2.png"
+import pulseira3 from "../assets/produtos/pulseira3.png"
+
 
 import Postagem from "../components/Postagem";
 
+interface Produto {
+  nome: string
+  preco: number
+  alt: string
+  img:unknown
+}
+
+const aneis: Produto[] = [
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel1
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel2
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel3
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel4
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel4
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel4
+  },
+  {
+    nome: "Anel Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: anel4
+  },
+]
+const pulseiras: Produto[] = [
+  {
+    nome: "Pulseira Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: pulseira1
+  }, 
+  {
+    nome: "Pulseira s",
+    preco: 199.99,
+    alt: "anelouro",
+    img: pulseira2
+  }, 
+  {
+    nome: "Pulseira Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: pulseira3
+  }, 
+]
+const colares: Produto[] = [
+  {
+    nome: "Pulseira Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: colar1
+  },
+  {
+    nome: "Pulseira Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: colar2
+  },
+  {
+    nome: "Pulseira Ouro",
+    preco: 199.99,
+    alt: "anelouro",
+    img: colar3
+  }
+]
+
 const listMenu = [
-  { nome: "Aneis", imagem: anel },
-  { nome: "Pulseira", imagem: pulseira },
-  { nome: "Colar", imagem: colar },
+  { nome: "Aneis", imagem: aneis },
+  { nome: "Pulseira", imagem: pulseiras },
+  { nome: "Colar", imagem: colares },
 ];
 
 export const Home = () => {
+
   const [escolha, setEscolha] = useState(null);
-  const bundleButton = (data) => {
-    setEscolha(data);
-  };
+  
   return (
     <>
       <div className="h-full w-full">
         <div className="bg-[rebeccapurple] items-center min-h-[40vh] text-6xl flex justify-center text-white font-thin">
           Florecer
         </div>
+
+
         <div className="bg-gray-200 flex space-x-4 min-h-[60vh]">
-          <div className="bg-purple-300 flex items-center justify-center text-white flex-col px-10 text-2xl font-thin">
+          <div className="bg-purple-300 flex items-center justify-center text-white flex-col xl:px-10 p-4 text-2xl font-thin">
             <li className="flex flex-col items-center">
               {listMenu.map((data) => (
                 <a key={data.nome}>
                   <button
                     className="p-2 hover:bg-slate-400"
                     onClick={() => {
-                      bundleButton(data);
+                      setEscolha(data);
                     }}
                   >
                     {data.nome}
@@ -39,7 +152,9 @@ export const Home = () => {
               ))}
             </li>
           </div>
-          <div className="flex justify-center w-full  items-center">
+
+
+          <div className="flex justify-center w-full items-center">
             {escolha ? (
               <>
                 <Postagem data={escolha} />
